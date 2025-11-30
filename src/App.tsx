cbf64dcc164586e4bdcf015b03ec11924791f3ff
Coded_mind__ <shahzadekhan296@gmail.com>
2025-11-30 21:29:53 +0530

    Make responsive and also add typing animation in heading.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import { Typewriter } from 'react-simple-typewriter'
 
 function App() {
 
 
   return (
     <>
-      <div className='flex h-[10%]  w-[100%] flex items-center  '>
+      <div className='flex  h-[10%]  w-[100%] flex items-center  '>
         <Navbar />
       </div>
-      <h1  className='flex mt-[4%] text-[#8603dc]  text-3xl justify-center font-medium '>
+      <h1 className='flex mt-[4%] text-[#8603dc] max-[425px]:text-[10px] max-[350px]:text-[9px] text-xs sm:text-xs md:text-xl lg:text-2xl  w-[100%] justify-center font-medium '>
         Hi, I'm Coded_Mind__
-        <span className='text-[#0bd9c8]'>— a curious mind crafting meaningful digital experiences.
+        <span className='text-[#0bd9c8]'>
+          <Typewriter
+            words={[`— a curious mind crafting meaningful digital experiences.`]}
+            loop={false}
+            cursor={true}
+            cursorStyle="|"
+            typeSpeed={50}
+            deleteSpeed={50}
+            delaySpeed={100000}
+
+          />
         </span>
       </h1>
       <div></div>
