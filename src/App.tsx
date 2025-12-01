f33b25939fdb5a8ccb70b817c1cc3163fd843aad
Coded_mind__ <shahzadekhan296@gmail.com>
2025-12-01 13:39:41 +0530

    50% techstack showcase section completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import { Typewriter } from 'react-simple-typewriter'
+import Techstack from './components/techstack'
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
       <div className='flex  h-[10%]  w-[100%] flex items-center  '>
         <Navbar />
       </div>
-      <h1 className='flex mt-[4%] text-[#8603dc] max-[425px]:text-[10px] max-[350px]:text-[9px] text-xs sm:text-xs md:text-xl lg:text-2xl  w-[100%] justify-center font-medium '>
+      <h1 className='flex mt-[4%] max-[480px]:p-0  p-7 text-[#8603dc] max-[425px]:text-[10px] max-[350px]:text-[9px] text-xs sm:text-[16px] md:text-xl lg:text-2xl  w-[100%] justify-center font-medium '>
         Hi, I'm Coded_Mind__
         <span className='text-[#0bd9c8]'>
           <Typewriter
@@ -21,11 +22,12 @@ function App() {
             typeSpeed={50}
             deleteSpeed={50}
             delaySpeed={100000}
-
           />
         </span>
       </h1>
-      <div></div>
+      <div className='gap-7  h-[20%] flex justify-center items-center max-[480px]:mt-6'>
+        <Techstack />
+      </div>
     </>
   )
 }
--- src/components/techstack.tsx
+++ src/components/techstack.tsx
@@ -1,6 +1,20 @@
+import { SiReact, SiTailwindcss, SiTsnode, SiPython, SiExpress, SiPostgresql, SiMongodb } from 'react-icons/si'
+
+
 function techstack() {
   return (
-    <div>techstack</div>
+    <div className='max-[1350px]:w-[50%] max-[1000px]:w-[60%] max-[850px]:w-[70%] max-[650px]:w-[90%] max-[520px]:w-[100%] text-4xl flex  h-[100%] items-center justify-center gap-[1%] w-[40%]'>
+      <SiReact className="relative  text-[#08dcff] top-[15%] right-[1%] animate-bounce" />
+      <SiTailwindcss className="relative  text-[#1dc0cd] top-[-20%] right-[-5%] animate-bounce" />
+      <SiTsnode className='text-[#2966ab] max-[650px]:top-[-35%] relative animate-bounce top-[-24.5%] right-[24%]  ' />
+      <SiPython className='text-[#f7ce43] relative animate-bounce top-[-12.5%] right-[-10%] ' />
+      <SiExpress className='text-[#fcfaf5] max-[650px]:right-[54%] relative animate-bounce top-[-7.5%] right-[53%] ' />
+      <SiPostgresql className='text-[#376695] relative animate-bounce top-[5%] right-[-12%] ' />
+      <SiMongodb className='text-[#08ec68] relative animate-bounce top-[20%] right-[16%] max-[650px]:right-[30%] ' />
+
+
+    </div>
+
   )
 }
 
