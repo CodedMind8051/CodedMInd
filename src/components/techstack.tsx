fa46f570c851bd4ca2ce0e0c5c29fc952a509e16
Coded_mind__ <shahzadekhan296@gmail.com>
2025-12-01 20:02:16 +0530

    Styling need improvement in textstack showcase section.

--- src/components/techstack.tsx
+++ src/components/techstack.tsx
@@ -1,18 +1,23 @@
-import { SiReact, SiTailwindcss, SiTsnode, SiPython, SiExpress, SiPostgresql, SiMongodb } from 'react-icons/si'
+import { SiReact, SiAndroid, SiTailwindcss, SiGit, SiFastapi, SiDjango, SiDocker, SiAmazonwebservices, SiNodedotjs, SiTypescript, SiPython, SiExpress, SiPostgresql, SiMongodb } from 'react-icons/si'
 
 
 function techstack() {
   return (
-    <div className='max-[1350px]:w-[50%] max-[1000px]:w-[60%] max-[850px]:w-[70%] max-[650px]:w-[90%] max-[520px]:w-[100%] text-4xl flex  h-[100%] items-center justify-center gap-[1%] w-[40%]'>
-      <SiReact className="relative  text-[#08dcff] top-[15%] right-[1%] animate-bounce" />
-      <SiTailwindcss className="relative  text-[#1dc0cd] top-[-20%] right-[-5%] animate-bounce" />
-      <SiTsnode className='text-[#2966ab] max-[650px]:top-[-35%] relative animate-bounce top-[-24.5%] right-[24%]  ' />
-      <SiPython className='text-[#f7ce43] relative animate-bounce top-[-12.5%] right-[-10%] ' />
-      <SiExpress className='text-[#fcfaf5] max-[650px]:right-[54%] relative animate-bounce top-[-7.5%] right-[53%] ' />
-      <SiPostgresql className='text-[#376695] relative animate-bounce top-[5%] right-[-12%] ' />
-      <SiMongodb className='text-[#08ec68] relative animate-bounce top-[20%] right-[16%] max-[650px]:right-[30%] ' />
-
-
+    <div className='border shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-md border-b-gray-800  bg-[#0f0f0f] rounded-3xl flex-wrap  max-[1350px]:w-[50%] max-[1000px]:w-[60%] max-[850px]:w-[70%] max-[650px]:w-[90%] max-[520px]:w-[100%] text-4xl flex   h-[100%] items-center justify-center gap-[7%] w-[40%]'>
+      <SiReact className="  text-[#08dcff]  animate-bounce" />
+      <SiTailwindcss className="  text-[#1dc0cd]  animate-bounce" />
+      <SiNodedotjs className='text-[#85c00a]   animate-bounce  ' />
+      <SiPython className='text-[#f7ce43]  animate-bounce ' />
+      <SiExpress className='text-[#fcfaf5]  animate-bounce  ' />
+      <SiPostgresql className='text-[#376695]  animate-bounce ' />
+      <SiMongodb className='text-[#08ec68]  animate-bounce  ' />
+      <SiTypescript className='text-[#3178c6]  animate-bounce ' />
+      <SiGit className='text-[#f05032]  animate-bounce ' />
+      <SiDocker className='text-[#2496ed]  animate-bounce ' />
+      <SiAmazonwebservices className='text-[#ff9900]  animate-bounce ' />
+      <SiFastapi className='text-[#009688]  animate-bounce ' />
+      <SiDjango className='text-[#096e48]  animate-bounce ' />
+      <SiAndroid className='text-[#3ddc84]  animate-bounce ' />
     </div>
 
   )
