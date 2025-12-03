7e70cd32f030226e7699979d8fed0fe910443cf3
Coded_mind__ <shahzadekhan296@gmail.com>
2025-12-03 11:46:29 +0530

    Added project img section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ function Navbar() {
         <nav className="w-[100%]  flex items-center justify-between h-[50%] ">
             <img className='w-[60px] sm:w-[80px] md:w-[80px] xl:w-[160px] lg:w-[150px] m-[10%] mb-[8%] rounded-[100%] ' src={Logo} alt="" />
             <div className="navlinks flex gap-[3%] max-[500px]:gap-[5%] mt-[2%] font-medium text-xs sm:text-xl lg:text-2xl items-center justify-center  w-[50%]">
-                <a className="text-white" href="#">Home</a>
-                <a className="text-white" href="#">Projects</a>
-                <a className="text-white" href="#">About</a>
-                <a className="text-white" href="#">Contact</a>
+                <a className="text-white hover:underline hover:decoration-2 decoration-white" href="#">Home</a>
+                <a className="text-white hover:underline hover:decoration-2 decoration-white" href="#">Projects</a>
+                <a className="text-white hover:underline hover:decoration-2 decoration-white" href="#">About</a>
+                <a className="text-white hover:underline hover:decoration-2 decoration-white" href="#">Contact</a>
             </div>
         </nav>
     )
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,22 +1,36 @@
 import { useState, } from "react"
+import { motion } from 'framer-motion'
+import Projectlogo from '../assets/project.png'
+import ComingSoon from '../assets/comingSoon.png'
 
 function Projects() {
-    const [Cards, setCards] = useState<any>([])
+    const [Cards] = useState<any>([])
+    const [ProjectDetail] = useState<any>([{ Projectlogo, Name: "Forkyou" }])
 
     for (let index = 0; index < 4; index++) {
-        Cards.push(
+        try {
+            Cards.push(
+                <motion.div whileHover={{ scale: 1.1, boxShadow: "0px 10px 30px rgba(0,0,0,0.3)" }} transition={{ duration: 0.5, ease: "easeOut" }} className=" card hover:cursor-pointer  ">
+                    <div className=" overflow-hidden   rounded h-[45%] w-full">
+                        <img src={ProjectDetail[index].Projectlogo} className="h-full rounded rounded-b-xl w-full " alt="" />
+                    </div>
+                </motion.div>)
+        } catch (error) {
+               Cards.push(
+                <motion.div whileHover={{ scale: 1.1, boxShadow: "0px 10px 30px rgba(0,0,0,0.3)" }} transition={{ duration: 0.5, ease: "easeOut" }} className=" card hover:cursor-pointer  ">
+                    <div className=" overflow-hidden   rounded h-[45%] w-full">
+                        <img src={ComingSoon} className="h-full rounded rounded-b-xl w-full " alt="" />
+                    </div>
+                </motion.div>)
+        }
 
-                <div  className=" card   ">
-                    <div className="p-1.5 bg-gradient-to-r from-[#ed59c0] via-[#e7039b] to-[#710ab8] rounded h-[45%] w-full">hii</div>
-                </div>)
     }
 
     return (
         <>
-            <div className=" gap-3.5 max-[1050px]:w-[90%] max-[700px]:w-[100%] max-[700px]:h-[70%]  flex justify-center h-[100%] flex-wrap w-[60%] text-2xl max-[700px]:text-xl font-medium text-white">
-                <h1 className=" justify-center flex items-center w-[100%] h-[5%] mt-2.5 underline  ml-2 text-[#f8f5f9]">Projects</h1>
+            <div className=" gap-6.5 max-[1050px]:w-[90%] max-[700px]:w-[100%] max-[700px]:h-[70%]  flex justify-center h-[100%] flex-wrap w-[60%] text-2xl max-[700px]:text-xl font-medium text-white">
+                <h1 className="decoration-2 decoration-white justify-center flex items-center w-[100%] h-[5%] mt-2.5 underline  ml-2 text-[#f8f5f9]">Projects</h1>
                 {Cards}
-         
             </div>
 
 
