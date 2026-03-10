4203a3c2f8f47fefe9bfd991f0b4504fcdf5c135
Coded_Mind__ <shahzadekhan296@gmail.com>
2026-03-10 20:56:54 +0530

    Make it SEO friendly by updating index.html and remove all comments from all files.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import './index.css'
 import App from './App.tsx'
 // @ts-ignore
 import Oneko from 'react-cursor-cat-codedmind';
+import ReactGA from 'react-ga4';
+   
+ReactGA.initialize('G-DY8YW4V94J');
 
 createRoot(document.getElementById('root')!).render(
     <>
